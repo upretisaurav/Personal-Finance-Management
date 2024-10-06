5a2e3339f403b4122bef9b999c0498050ee8b0b0
Saurav <Upretisaurav7@gmail.com>
2024-10-06 17:48:26 +0545

    Removed tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Welcome from "./pages/Welcome";
 import Navbar from "./components/common/Navbar";
-import ExpenseManagement from "./pages/ExpenseManagement";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/welcome" element={<Welcome />} />
-        <Route path="/expense" element={<ExpenseManagement />} />
       </Routes>
     </Router>
   );
